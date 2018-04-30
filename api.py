45b7f41705048d2c4986c2f8494d681e15719fc2
BTx123 <btom.0831@gmail.com>
2018-04-30 16:25:42 -0700

    Update pin controller numbering

--- api.py
+++ api.py
@@ -8,12 +8,14 @@ ROUTE_API = "/api"
 ROUTE_LEDS = "/leds"
 DEBUG = True
 
-#create a todo list ie list of the id and states of each pin, call the state and id in LED Controller, make a class for each pin, and then call it 
-leds = {17: LEDController(17) }
+# create a todo list ie list of the id and states of each pin, call the state and id in LED Controller, make a class for each pin, and then call it
+VALID_LEDS = [11]
+leds = {led: LEDController(led) for led in VALID_LEDS}
+
 
 def abort_if_LED_invalid(led_pin: int):
     """Abort the request if invalid pin is chosen and give 404."""
-    if led_pin not in leds:  # TODO: update condition
+    if led_pin not in VALID_LEDS:  # TODO: update condition
         abort(404, message="Pin {pin} not available.".format(pin=led_pin))
 
 
@@ -31,7 +33,7 @@ class LED(Resource):
     def get(self, led_pin: int) -> dict:
         """GET pin state."""
         abort_if_LED_invalid(led_pin)
-        state = leds[17].get_state()
+        state = leds[led_pin].get_state()
         logging.debug("GET: pin {pin} = {state}".format(
             pin=led_pin, state=state))
         return {
@@ -44,21 +46,24 @@ class LED(Resource):
         abort_if_LED_invalid(led_pin)
         logging.debug("POST: {command} pin {pin}".format(
             command=command, pin=led_pin))
-        #check for on, off, toggle
-        if command == "on": leds[led_pin].on() 
-        elif command == "off": leds[led_pin].off()
-        elif command == "toggle": leds[led_pin].toggle()
-        else: abort (404, message="Command not available.")
+        # check for on, off, toggle
+        if command == "on":
+            leds[led_pin].on()
+        elif command == "off":
+            leds[led_pin].off()
+        elif command == "toggle":
+            leds[led_pin].toggle()
+        else:
+            abort(404, message="Command not available.")
         return {
             "pin": led_pin,
             "state": leds[led_pin].get_state()
         }
 
 
-class LEDS(Resource): 
-    def get(self): 
+class LEDS(Resource):
+    def get(self):
         return str(leds)
-
 
 
 def main():
@@ -69,27 +74,22 @@ def main():
     else:
         logger.setLevel(logging.CRITICAL)
 
-
     # Flask application
     app = Flask(__name__)
     api = Api(app)
-
-
 
     # Add resources (API endpoints)
     api.add_resource(HelloWorld, "/api")
 
     api.add_resource(LED, ROUTE_API + ROUTE_LEDS + "/<int:led_pin>")
 
-
-    #test 
-    api.add_resource(LED,  ROUTE_API + ROUTE_LEDS + "/<int:led_pin>/<string:command>", endpoint="turn on LED by pin")
+    # test
+    api.add_resource(LED,  ROUTE_API + ROUTE_LEDS +
+                     "/<int:led_pin>/<string:command>", endpoint="turn on LED by pin")
 
     # Run the app
     #app.run(host="0.0.0.0", port=5000, debug=DEBUG)
     app.run(debug=DEBUG)
-
-
 
 
 if __name__ == "__main__":
